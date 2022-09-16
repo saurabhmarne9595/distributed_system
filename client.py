0aa4b54ef0d39131dd35045d7c78edc855b45805
saurabhmarne9595 <saurabhmarne9595@gmail.com>
2022-09-15 22:53:30 -0500

    Added dir

--- client.py
+++
@@ -1,7 +0,0 @@
-import xmlrpc.client
-
-with xmlrpc.client.ServerProxy("http://localhost:8000/") as proxy:
-    
-    print("sumation of 2 and 3 =", proxy.add(2,4))
-    li = [3,6,0,1,8,4,8,57]
-    print("listed sorted on RPC - ", proxy.sort_list(li))
